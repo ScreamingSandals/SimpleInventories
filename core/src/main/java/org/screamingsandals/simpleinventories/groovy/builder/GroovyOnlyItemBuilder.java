d0c5988f5d34f6c75f9a0b0c8f033115f3cbece3
Misat11 <misa.turek1@gmail.com>
2020-05-29 17:33:19 +0200

    implementing cosmetic, hidden, include and insert inside insert builder in groovy

--- core/src/main/java/org/screamingsandals/simpleinventories/groovy/builder/GroovyOnlyItemBuilder.java
+++ core/src/main/java/org/screamingsandals/simpleinventories/groovy/builder/GroovyOnlyItemBuilder.java
@@ -5,9 +5,7 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import org.bukkit.Material;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 import static org.screamingsandals.simpleinventories.groovy.utils.GroovyUtils.internalCallClosure;
 
@@ -46,6 +44,86 @@ public class GroovyOnlyItemBuilder {
 
     public void item(Material material, Closure<GroovyItemBuilder> closure) {
         internalCallClosure(closure, new GroovyItemBuilder(internalAddItem(material)));
+    }
+
+    public void cosmetic() {
+        internalAddItem(new HashMap<String, Object>() {
+            {
+                put("clone", "cosmetic");
+            }
+        });
+    }
+
+    public void cosmetic(Closure<GroovyItemBuilder> closure) {
+        Map<String, Object> map = new HashMap<String, Object>() {
+            {
+                put("clone", "cosmetic");
+            }
+        };
+        internalAddItem(map);
+        internalCallClosure(closure, new GroovyItemBuilder(map));
+    }
+
+    public void itemClone(String clone) {
+        internalAddItem(new HashMap<String, Object>() {
+            {
+                put("clone", clone);
+            }
+        });
+    }
+
+    public void itemClone(String clone, Closure<GroovyItemBuilder> closure) {
+        Map<String, Object> map = new HashMap<String, Object>() {
+            {
+                put("clone", clone);
+            }
+        };
+        internalAddItem(map);
+        internalCallClosure(closure, new GroovyItemBuilder(map));
+    }
+
+    public void include(String include) {
+        internalAddItem(new HashMap<String, Object>() {
+            {
+                put("include", include);
+            }
+        });
+    }
+
+    public void hidden(String id, Closure<GroovyOnlyItemBuilder> closure) {
+        List<Object> items = new ArrayList<>();
+        internalAddItem(new HashMap<String, Object>() {
+            {
+                put("id", id);
+                put("write", false);
+                put("items", items);
+            }
+        });
+        internalCallClosure(closure, new GroovyOnlyItemBuilder(items));
+    }
+
+    public void hidden(String id) {
+        internalAddItem(new HashMap<String, Object>() {
+            {
+                put("id", id);
+                put("write", false);
+            }
+        });
+    }
+
+    public void insert(String insert, Closure<GroovyOnlyItemBuilder> closure) {
+        insert(Collections.singletonList(insert), closure);
+    }
+
+    public void insert(List<String> insert, Closure<GroovyOnlyItemBuilder> closure) {
+        List<Object> items = new ArrayList<>();
+        internalAddItem(new HashMap<String, Object>() {
+            {
+                put("insert", insert);
+                put("items", items);
+            }
+        });
+        internalCallClosure(closure, new GroovyOnlyItemBuilder(items));
     }
 
     private Map<String, Object> internalAddItem(Object material) {
